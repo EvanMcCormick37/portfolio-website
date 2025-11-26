7cc2eb59017df1e359fea6aee007bf77f7801280
EvanMcCormick37 <e.kidmccorm@gmail.com>
2025-11-25 23:04:27 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -149,6 +149,16 @@ const ProjectCard=({ project })=> {
           >
             <span>Read More</span>
           </a>
+          {project.liveUrl && (
+          <a
+            href={project.liveUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary"
+            >
+            <span>Try it Live!</span>
+          </a>
+          )}
           <a
             href={project.githubUrl}
             target="_blank"
